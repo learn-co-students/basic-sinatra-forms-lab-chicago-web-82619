a84136c580a3d2b68211df0d997720ae46b9c7a6
aaron blum <blumaa@gmail.com>
2019-09-17 11:38:17 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+#this block shows the newteam form
+  get '/newteam' do
+    erb :newteam
+  end
+
+  #this block posts the newteam form
+  post '/team' do
+    p params
+    erb :team
+  end
+
+  get '/team' do
+    erb :team
+  end
 
 end
